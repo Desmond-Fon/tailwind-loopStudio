900d6af0c1ba671e996827c22ddc5685b3dc5b89
Desmond-Fon <desmondfon@yahoo.com>
2022-10-15 23:00:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*/*.html',],
+  theme: {
+    extend: {
+      fontFamily: {
+         Josefin :['Josefin Sans', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
